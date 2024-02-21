8bc86a50c7f0201fb353e301a1140dca14eaafb8
DiegoDelvig <157409019+DiegoDelvig@users.noreply.github.com>
2024-02-21 16:57:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 import string
 import os
+import sys
 import random
 
 
 def get_random_wallpaper(query: str = '', page: int = 1, nsfw: bool = False) -> str:
+    """ Use wallhaven.cc API and take a random wallpaper using different arguments. Return the wallpaper download link. """
     if nsfw:
         purity = 111
     else:
@@ -27,11 +29,13 @@ def get_random_wallpaper(query: str = '', page: int = 1, nsfw: bool = False) -> 
 
 
 def generate_id(length: int) -> str:
+    """ generate a random combination of length-lowercase string. Return the formated string. """
     id = ''.join(random.choice(string.ascii_lowercase) for i in range(length))
     return f"wallpaper-{id}"
 
 
 def download_wallpaper(wallpaper_link: str, filename: str):
+    """ Take a download url and a name for the file. Download the image. """
     img_data = requests.get(wallpaper_link).content
     
     with open(f'wallpapers/{filename}.jpg', 'wb') as handler: 
@@ -39,17 +43,21 @@ def download_wallpaper(wallpaper_link: str, filename: str):
 
 
 if __name__ == "__main__":
-    wallpaper_link = get_random_wallpaper(page=random.randint(1, 3), nsfw=False)
+        
+    args = ['new', 'change'] 
 
-    filename = generate_id(6)
-    download_wallpaper(wallpaper_link, filename)
-    random.choice(os.listdir('/home/pavel/prog/wallpaper-generator/wallpapers'))
-    os.system(f"gsettings set org.gnome.desktop.background picture-uri 'file:///home/pavel/prog/wallpaper-generator/wallpapers/{filename}.jpg'")
+    if len(sys.argv) == 1:
+        print(f'argugment required: {args}')
 
-
-
-
-
+    elif sys.argv[1] == args[0]:
+        wallpaper_link = get_random_wallpaper(query="ghibli", page=random.randint(1, 3), nsfw=False)
+        filename = generate_id(6)
+        download_wallpaper(wallpaper_link, filename)
+        os.system(f"gsettings set org.gnome.desktop.background picture-uri 'file:///home/pavel/prog/wallpaper-generator/wallpapers/{filename}.jpg'")
+    
+    elif sys.argv[1] == args[1]:
+        filename = random.choice(os.listdir('/home/pavel/prog/wallpaper-generator/wallpapers'))
+        os.system(f"gsettings set org.gnome.desktop.background picture-uri 'file:///home/pavel/prog/wallpaper-generator/wallpapers/{filename}'")
 
 
 
